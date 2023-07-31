aed5e1b29df9dc04167a2e40c41b5709ef663f92
Mohammed Alsahli <mohammed.saud.alsahli@gmail.com>
2023-07-31 20:11:56 +0300

    added the homework

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,13 +5,24 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Internet Images'),
-      ),
-      body: Container(
-        child: const Column(
-          children: [],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey[800],
+        appBar: AppBar(
+          title: const Text('Internet images'),
+        ),
+        body: Center(
+          child: Container(
+            width: 300,
+            height: 400,
+            child: Column(
+              children: [
+                Image.network(
+                    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.1YM53mG10H_U25iPjop83QHaEo%26pid%3DApi&f=1&ipt=e8b1b8707640785ba13fdef737422182d0660550ec34619e444b0051524534e1&ipo=images'),
+                Image.asset('assets/image1.jpg'),
+              ],
+            ),
+          ),
         ),
       ),
     );
